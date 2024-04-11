360925d065fb90ed04d4a59a2efbf635d63ad187
13mk-baig <13mk-baig@gmail.com>
2024-04-11 18:14:18 +0500

    text change

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-  { message: "enter first number", type: "number", name: "firstnumber" },
-  { message: "enter second number", type: "number", name: "secondnumber" },
+  { message: "Enter first number", type: "number", name: "firstnumber" },
+  { message: "Enter second number", type: "number", name: "secondnumber" },
 
   {
     message: "select one of the operator to perform action",
